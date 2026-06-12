59d7d92bbe638f75b0a2f0bb0b87462ba673eab9
Vijaya Sundar <vijayasundar.b@gmail.com>
2026-06-12 07:05:15 +0530

    fix gemini api call

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({
-    model: "/gemini-1.5-flash"
+    model: "/gemini-2.5-flash"
 });
 
 
